db4229c9b263ef2818c7847b01b3ae1d66c46049
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-06-22 12:26:04 +0100

    Refactor SearchPage to create cards from object

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import {exampleJSON} from '../../helpers/exampleJson'
 import vehicleOne from '../../assets/images/vehicle_image_one.svg'
+import vehicleTwo from '../../assets/images/vehicle_image_two.svg'
+import vehicleThree from '../../assets/images/vehicle_image_three.svg'
 import MobileVehicleCard from '../../components/MobileVehicleCard'
 
 
@@ -23,21 +25,28 @@ const VehicleListContainer = styled.div`
 
 
 const SearchPage = () => {
+
+    const vehiclesToRender = [
+        {id: 10, image: vehicleOne, vehicleInfo: exampleJSON},
+        {id: 11, image: vehicleTwo, vehicleInfo: exampleJSON},
+        {id: 12, image: vehicleThree, vehicleInfo: exampleJSON},
+        {id: 13, image: vehicleOne, vehicleInfo: exampleJSON},
+        {id: 14, image: vehicleTwo, vehicleInfo: exampleJSON},
+        {id: 15, image: vehicleThree, vehicleInfo: exampleJSON},
+    ]
+
+    const renderMobileCards = () => vehiclesToRender.map(vehicle => (
+        <MobileVehicleCard
+            image={vehicle.image}
+            key={vehicle.id}
+            vehicle={vehicle.vehicleInfo}
+        />
+    ))
+
     return (
         <PageContainer>
             <VehicleListContainer>
-                <MobileVehicleCard
-                    image={vehicleOne}
-                    vehicle={exampleJSON}
-                />
-                <MobileVehicleCard
-                    image={vehicleOne}
-                    vehicle={exampleJSON}
-                />
-                <MobileVehicleCard
-                    image={vehicleOne}
-                    vehicle={exampleJSON}
-                />
+                {renderMobileCards()}
             </VehicleListContainer>
         </PageContainer>
     )
